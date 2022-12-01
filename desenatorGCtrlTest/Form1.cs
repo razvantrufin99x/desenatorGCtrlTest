87a602cce2b9451a048b93311ba1e8c8115bc63e
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 23:10:11 +0200

    Add project files.

---
+++ desenatorGCtrlTest/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using graphiccontrol;
+
+namespace desenatorGCtrlTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+              }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.userControl11.g.DrawEllipse(this.userControl11.pen0, 100, 100, 220, 210);
+     
+        }
+    }
+}
